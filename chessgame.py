9abce5982d57334e77dac7e85790396e1693802c
Jonathanace <jonathanace@gmail.com>
2022-11-15 14:43:13 -0800

    Implemented member functions to position class to change position data and return possible moves. Added function in pawn to check for valid moves using position functions.

--- chessgame.py
+++ chessgame.py
@@ -1,19 +1,54 @@
 class ChessGame:
-    def __init__(self, whitePlayer, blackPlayer, board=Board()):
+    def __init__(self, whitePlayer, blackPlayer):
         self.white = whitePlayer
         self.black = blackPlayer
-        self.board = board
+        self.board = Board()
 
 class Player:
     pass
 
 class Board:
-    pass
+    def __init__(self):
+        pass
 
 class Position:
     def __init__(self, pos):
-        self.rank = pos[1]
-        self.file = pos[0]
+        self.FILE_TO_NUM = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8}
+        self.NUM_TO_FILE = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h'}
+        self.VALID_POSITIONS = {
+            'a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8',
+            'a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7',
+            'a6', 'b6', 'c6', 'd6', 'e6', 'f6', 'g6', 'h6',
+            'a5', 'b5', 'c5', 'd5', 'e5', 'f5', 'g5', 'h5',
+            'a4', 'b4', 'c4', 'd4', 'e4', 'f4', 'g4', 'h4',
+            'a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3', 'h3',
+            'a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2',
+            'a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1'
+        }
 
-    def output_pos(self):
-        return f'{file}{rank}'
+        self.rank = int(pos[1])
+        self.file = self.FILE_TO_NUM[pos[0]]
+
+    def output_pos(self, file, rank):
+        return f'{self.NUM_TO_FILE[file]}{rank}'
+
+    def output_cur_pos(self):
+        return self.output_pos(self.file, self.rank)
+
+    def get_directional(self, direction):
+        if direction == 1:
+            return self.output_pos(self.file-1, self.rank+1) if (self.file > 1 and self.rank < 8) else None # forward left
+        elif direction == 2:
+            return self.output_pos(self.file, self.rank+1) if self.rank < 8 else None # forward
+        elif direction == 3:
+            return self.output_pos(self.file+1, self.rank+1) if (self.file < 8 and self.rank) < 8 else None # forward right
+        elif direction == 4: 
+            return self.output_pos(self.file+1, self.rank) if self.file < 8 else None # right
+        elif direction == 5:
+            return self.output_pos(self.file+1, self.rank-1) if (self.file < 8 and self.rank > 1) else None # back right
+        elif direction == 6:
+            return self.output_pos(self.file, self.rank-1) if self.rank > 1 else None # back
+        elif direction == 7:
+            return self.output_pos(self.file-1, self.rank-1) if (self.file > 1 and self.rank > 1) else None # back left
+        elif direction == 8:
+            return self.output_pos(self.file-1, self.rank) if self.file > 1 else None # left
--- pieces.py
+++ pieces.py
@@ -2,14 +2,28 @@ class Piece:
     def __init__(self, piece_pos, color):
         self.position = position(piece_pos)
         self.color = color
+        self.VALID_POSITIONS = {
+            'a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8',
+            'a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7',
+            'a6', 'b6', 'c6', 'd6', 'e6', 'f6', 'g6', 'h6',
+            'a5', 'b5', 'c5', 'd5', 'e5', 'f5', 'g5', 'h5',
+            'a4', 'b4', 'c4', 'd4', 'e4', 'f4', 'g4', 'h4',
+            'a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3', 'h3',
+            'a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2',
+            'a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1'
+        }
 
     def change_position(new_position):
         self.position = position(new_pos)
 
-
 class Pawn(Piece):
     def get_valid_moves(self):
-        
+        # if color is black multiply all possible positions by -1
+        if self.color == 'W':
+            return self.position.get_directional(2) # a white pawn can move forward
+        elif self.color == 'B':
+            return self.position.get_directional(6) # a black pawn can move backwards
+
 
 class Knight(Piece):
     pass
