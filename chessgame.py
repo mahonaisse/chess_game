db50ddbc92b1d470314f1ad5c59917321e348db6
lumpydumpling <cau.009@ucr.edu>
2022-11-15 18:19:05 -0800

    added menu func to chessgame class

--- chessgame.py
+++ chessgame.py
@@ -3,6 +3,39 @@ class ChessGame:
         self.white = whitePlayer
         self.black = blackPlayer
         self.board = Board()
+        
+    def show_menu(self):
+        typeGame = None
+        while True:
+            try:
+                typeGame = int(input("Enter 1 for New Game, 2 for Load Game, or 3 for Play Custom Games")) 
+                if typeGame=='1':
+                    print("New Game entered successfully")
+                    self.start_new_game()
+                    break
+                elif typeGame=='2':
+                    print("Load Game entered successfully")
+                    self.load_saved_game()
+                    break
+                elif typeGame=='3':
+                    print("Play Custom Games entered successfully")
+                    self.chess_variant()
+                    break
+                else:
+                    print("Enter 1, 2 or 3")      
+            except ValueError:
+                print("Provide an integer value...")
+                continue
+            
+    def start_new_game():
+        #reset pieces to initial position
+        pass
+        
+    def load_saved_game():
+        pass
+    
+    def chess_variant():
+        pass
 
 class Player:
     pass
