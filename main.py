c60fdd420eb9c0479d55e48db0743bdca5e96e90
mahonaisse <jonsquared4@gmail.com>
2022-11-28 21:49:38 -0800

    Added while loop to swap between players' turns and perpetuate the game until one player is checkmated.

--- main.py
+++ main.py
@@ -12,14 +12,22 @@ black_player = Player(black_players_name, 'B')
 displayBoard(white_player, black_player)
 
 move = None
+white_player_checkmated = black_player_checkmated = False
+
+current_players_name = white_players_name
+non_current_players_name = black_players_name
+
+current_player = white_player
+non_current_player = black_player
 
 # get initial move
-while not move:
-    name_of_piece_to_move = input(f'What piece do you want to move, {white_players_name}? ')
-    piece_to_move = white_player.findPieceByPieceName(name_of_piece_to_move)
+while (not white_player_checkmated and not black_player_checkmated) or not move:
+    print(f"It is {current_players_name}'s turn. ")
+    name_of_piece_to_move = input(f'What piece do you want to move, {current_players_name}? ')
+    piece_to_move = current_player.findPieceByPieceName(name_of_piece_to_move)
     while not piece_to_move:
         name_of_piece_to_move = input('Invalid piece. Please input valid piece name: ')
-        piece_to_move = white_player.findPieceByPieceName(name_of_piece_to_move)
+        piece_to_move = current_player.findPieceByPieceName(name_of_piece_to_move)
 
     target_rank = int(input('Enter target rank: '))
     while target_rank < 1 or target_rank > 8:
@@ -30,17 +38,18 @@ while not move:
         target_file = int(input('Target file is invalid. Please enter a valid file: '))
 
 
-    move = white_player.movePiece(name_of_piece_to_move, target_rank, target_file, black_player)
+    move = current_player.movePiece(name_of_piece_to_move, target_rank, target_file, non_current_player)
     if not move:
         print('Invalid move. Please try again.')
-last_piece_moved = name_of_piece_to_move
+
+    # swap current player and current player's name
+    if move:
+        current_player, non_current_player = non_current_player, current_player
+        current_players_name, non_current_players_name = non_current_players_name, current_players_name
 
 
-
-
-
-# white_player_checkmated = white_player.isCheckMate(black_player, white_player.findPieceByPieceName(piece_to_move))
-# black_player_checkmated = black_player.isCheckMate(white_player, black_player.findPieceByPieceName(piece_to_move))
+white_player_checkmated = white_player.isCheckMate(black_player, white_player.findPieceByPieceName(piece_to_move))
+black_player_checkmated = black_player.isCheckMate(white_player, black_player.findPieceByPieceName(piece_to_move))
 
 
 
