6804645aa4e55110a4d0b082b1bf9bb33a8c0503
mahonaisse <jonsquared4@gmail.com>
2022-11-28 21:16:52 -0800

    Added prompts for piece movement rank and file along with validity checks.

--- main.py
+++ main.py
@@ -17,4 +17,41 @@ while not piece_to_move:
     name_of_piece_to_move = input('Invalid piece. Please input valid piece name: ')
     piece_to_move = white_player.findPieceByPieceName(name_of_piece_to_move)
 
-# while not (player1.isCheckMate(player2, player1.findPieceByPieceName('Q')))
+target_rank = int(input('Enter target rank: '))
+while target_rank < 1 or target_rank > 8:
+    target_rank = int(input('Target rank is invalid. Please enter a valid rank: '))
+
+target_file = int(input('Enter target file: '))
+while target_file < 1 or target_file > 8:
+    target_file = int(input('Target file is invalid. Please enter a valid file: '))
+
+white_player_checkmated = white_player.isCheckMate(black_player, white_player.findPieceByPieceName(piece_to_move))
+black_player_checkmated = black_player.isCheckMate(white_player, black_player.findPieceByPieceName(piece_to_move))
+
+
+
+
+
+
+
+
+while not white_player_checkmated and not black_player_checkmated:
+    name_of_piece_to_move = input(f'What piece do you want to move, {black_players_name}? ')
+    piece_to_move = black_player.findPieceByPieceName(name_of_piece_to_move)
+    
+    while not piece_to_move:
+        name_of_piece_to_move = input('Invalid piece. Please input valid piece name: ')
+        piece_to_move = black_player.findPieceByPieceName(name_of_piece_to_move)
+
+    target_rank = int(input('Enter target rank: '))
+    while target_rank < 1 or target_rank > 8:
+        target_rank = int(input('Target rank is invalid. Please enter a valid rank: '))
+
+    target_file = int(input('Enter target file: '))
+    while target_file < 1 or target_file > 8:
+        target_file = int(input('Target file is invalid. Please enter a valid file: '))
+
+    white_player_checkmated = white_player.isCheckMate(black_player, white_player.findPieceByPieceName(piece_to_move))
+    black_player_checkmated = black_player.isCheckMate(white_player, black_player.findPieceByPieceName(piece_to_move))
+        
+    
