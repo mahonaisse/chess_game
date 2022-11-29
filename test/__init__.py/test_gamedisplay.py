5ff7d85734f8e4472c3419bb2a842306ccdd1426
lumpydumpling <cau.009@ucr.edu>
2022-11-29 13:37:36 -0800

    first pawn tests, buildboard not  defined error

---
+++ test/__init__.py/test_gamedisplay.py
@@ -0,0 +1,9 @@
+from pieces import Piece
+
+def show_menu_default()->None:
+    menu = show_menu()
+    assertTrue()
+
+def display_game_default()->None:
+    display = display_game()
+    assertTrue()
--- test/__init__.py/test_pieces.py
+++ test/__init__.py/test_pieces.py
@@ -1,9 +1,39 @@
-from pieces import Piece
+from board import * 
+from player import *
+from pieces import *
 
-def show_menu_default()->None:
-    menu = show_menu()
-    assertTrue()
+# import unittest
 
-def display_game_default()->None:
-    display = display_game()
-    assertTrue()
+def test_pawn_not_isValidMove()-> None:
+    player1 = Player("Charles", "W")
+    player2 = Player("Harry", "B")
+    
+    # board = buildBoard(player1,player2)
+    pawn2 = player1.findPieceByPieceName("p2")
+    assert pawn2.isValidMove(4, 2, player1, player2) == False 
+    #if not valid, expect capture, checkmate return check,
+    
+def test_pawn_isValidMove()-> None:
+    player1 = Player("Charles", "W")
+    player2 = Player("Harry", "B")
+    
+    # board = buildBoard(player1,player2)
+    pawn1 = player1.findPieceByPieceName("p1")
+    assert pawn1.isValidMove(4, 1, player1, player2) == True
+    
+def test_pawn_isValidMove_capture()-> None:
+    player1 = Player("Charles", "W")
+    player2 = Player("Harry", "B")
+    
+    # board = buildBoard(player1,player2)
+    pawn2 = player1.findPieceByPieceName("p2")
+    assert pawn2.isValidMove(4, 1, player1, player2) == "capture" 
+    
+#test other pieces- Knight, rook, queen
+    
+# player2.movePiece("p4", 5, 4, player1)
+# player1.movePiece("P3" , 4, 3, player2)
+# player2.movePiece("q", 4, 1, player1)
+
+
+
