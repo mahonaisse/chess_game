7c192ce0b2e8104cfd9244647a925fbd0048a48d
mahonaisse <jonsquared4@gmail.com>
2022-11-28 20:59:33 -0800

    Added piece prompt and validations

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from chessgame import ChessGame #, Player, Board
+from player import Player
+from board import displayBoard
 #from pieces import Position, Pieces
 #from menu import
 
@@ -7,3 +8,13 @@ white_player = Player(white_players_name, 'W')
 
 black_players_name = input("Enter black player's name: ")
 black_player = Player(black_players_name, 'B')
+
+displayBoard(white_player, black_player)
+
+name_of_piece_to_move = input(f'What piece do you want to move, {white_players_name}? ')
+piece_to_move = white_player.findPieceByPieceName(name_of_piece_to_move)
+while not piece_to_move:
+    name_of_piece_to_move = input('Invalid piece. Please input valid piece name: ')
+    piece_to_move = white_player.findPieceByPieceName(name_of_piece_to_move)
+
+# while not (player1.isCheckMate(player2, player1.findPieceByPieceName('Q')))
