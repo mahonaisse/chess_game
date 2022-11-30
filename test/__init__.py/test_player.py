a2bd56956c62c373561074645ce2858e973fb72d
lumpydumpling <cau.009@ucr.edu>
2022-11-29 17:21:01 -0800

    added movepiece tests

--- test/__init__.py/test_player.py
+++ test/__init__.py/test_player.py
@@ -6,16 +6,34 @@ def test_movePiece_stalemate()-> None:
     player1 = Player("Charles", "W")
     player2 = Player("Harry", "B")
     
-    bishop1 = player1.findPieceByPieceName("B1")
-    player1.movePiece("P4", 4, 4, player2)
-    assert bishop1.isValidMove(3, 5, player1, player2) == "Stalemate"
+    king = player1.findPieceByPieceName("K")
+    len(player1.pieces) == 1 
+    len(player2.pieces) == 1
+    assert king.movePiece(3, 5, player1, player2) == "Stalemate"
     
-def test_movePiece_()-> None:
+def test_movePiece_checkmate()-> None:
     player1 = Player("Charles", "W")
     player2 = Player("Harry", "B")
     
     bishop1 = player1.findPieceByPieceName("B1")
     player1.movePiece("P4", 4, 4, player2)
-    assert bishop1.isValidMove(3, 5, player1, player2) == True
+    opponent, checkingPiece
+    assert king.movePiece(3, 5, player1, player2) == "checkmate"
+    
+def test_movePiece_checked()-> None:
+    player1 = Player("Charles", "W")
+    player2 = Player("Harry", "B")
+    
+    bishop1 = player1.findPieceByPieceName("B1")
+    player1.movePiece("P4", 4, 4, player2)
+    assert bishop1.isValidMove(3, 5, player1, player2) == "checked"
+    
+def test_invalidmovePiece()-> None:
+    player1 = Player("Charles", "W")
+    player2 = Player("Harry", "B")
+    
+    assert player1.movePiece("P4", 4, 4, player2) == False
+    
+
 
 #true, false, checked, checkmate
