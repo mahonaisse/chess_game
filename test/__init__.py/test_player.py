25306ddb920a45737a09fa7ac8d98836abb55ce7
lumpydumpling <cau.009@ucr.edu>
2022-11-29 16:51:41 -0800

    errors

--- test/__init__.py/test_player.py
+++ test/__init__.py/test_player.py
@@ -2,16 +2,15 @@ from pieces import *
 from board import *
 import copy
 
-#FIXME
-def test_isCheckMate_True()-> None:
+def test_movePiece_stalemate()-> None:
     player1 = Player("Charles", "W")
     player2 = Player("Harry", "B")
     
     bishop1 = player1.findPieceByPieceName("B1")
     player1.movePiece("P4", 4, 4, player2)
-    assert bishop1.isValidMove(3, 5, player1, player2) == True
+    assert bishop1.isValidMove(3, 5, player1, player2) == "Stalemate"
     
-def test_isCheckMate_False()-> None:
+def test_movePiece_()-> None:
     player1 = Player("Charles", "W")
     player2 = Player("Harry", "B")
     
@@ -19,3 +18,4 @@ def test_isCheckMate_False()-> None:
     player1.movePiece("P4", 4, 4, player2)
     assert bishop1.isValidMove(3, 5, player1, player2) == True
 
+#true, false, checked, checkmate
